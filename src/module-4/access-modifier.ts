8baa4c2a9acbf902b9bcaeb534947e59441c2421
Ashraful610 <ashalam610@gmail.com>
2024-02-18 00:12:12 +0600

    access modifier is done

---
+++ src/module-4/access-modifier.ts
@@ -0,0 +1,22 @@
+class BankAccount {
+  readonly   id:number;
+    name:string;
+ private   balance:number;
+    constructor(id:number, name:string , balance:number){
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    getBalance(){
+        console.log(`This balance is ${this.balance}`)
+    }
+
+    addBalance(balance:number){
+        console.log('new balance is added')
+    }
+}
+
+const myAccount = new BankAccount(5 , 'Ashraf' , 5000)
+// myAccount.balance = 1000
+console.log(myAccount)
